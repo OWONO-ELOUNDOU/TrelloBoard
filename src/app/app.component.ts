fd4f3228e9ef607776e3e943ceaa476b2038009d
OWONO ELOUNDOU <OWONO ELOUNDOU>
2024-09-07 13:52:58 +0100

    routes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { GravatarModule } from 'ngx-gravatar';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    GravatarModule,
+    NgxSpinnerModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { WorkspaceComponent } from './components/inside/workspace/workspace.component';
+import { BoardComponent } from './components/inside/board/board.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: LoginComponent,
+        title: "Login Page"
+    },
+    {
+        path: "workspace",
+        component: WorkspaceComponent,
+        title: "workspace Page",
+    },
+    {
+        path: "workspace/:id",
+        component: BoardComponent,
+        title: "workspace/board Page"
+    },
+    {
+        path: "**",
+        redirectTo: "/",
+    },
+];
